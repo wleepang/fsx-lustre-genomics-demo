e601850f60768ea71eb5faeff29070a11d413d2f
Pang, Lee <pwyming@.amazon.com>
2019-06-07 09:23:09 -0700

    fix region name

--- containers/bcftools/bcftools.aws.sh
+++ containers/bcftools/bcftools.aws.sh
@@ -30,7 +30,7 @@ function call() {
     bcftools call \
         -m \
         --threads 16 \
-        -r 21 \
+        -r chr21 \
         -o $OUTPUT_PATH/${SAMPLE_ID}.vcf \
         $INPUT_PATH/${SAMPLE_ID}.mpileup.vcf.gz
 }
@@ -39,7 +39,7 @@ function mpileup() {
     bcftools mpileup \
         --threads 16 \
         -Oz \
-        -r 21 \
+        -r chr21 \
         -f $REFERENCE_PATH/${REFERENCE_NAME}.fasta \
         $INPUT_PATH/${SAMPLE_ID}.bam \
         > $OUTPUT_PATH/${SAMPLE_ID}.mpileup.vcf.gz
